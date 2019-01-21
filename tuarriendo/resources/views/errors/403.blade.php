0da884f6938e681cebdd85ccb4527b74e84a5712
samucoc <samu.silva@gmail.com>
2019-01-21 14:37:14 -0300

    delete tuarriendo

--- tuarriendo/resources/views/errors/403.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@extends('errors::layout')
-
-@section('title', '403')
-
-@section('message', 'Acceso no autorizado')
--- tuarriendo/resources/views/errors/404.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@extends('errors::layout')
-
-@section('title', '404')
-
-@section('message', 'No se encuentra esa URL')
