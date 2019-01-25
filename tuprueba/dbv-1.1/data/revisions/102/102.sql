66fec869a677ad1bce63fd2b537c6d40c8276b27
samucoc <samu.silva@gmail.com>
2019-01-25 13:29:32 -0300

    Update 102.sql

--- tuprueba/dbv-1.1/data/revisions/102/102.sql
+++ tuprueba/dbv-1.1/data/revisions/102/102.sql
@@ -1,11 +1,25 @@
 INSERT INTO `menus_hijos` ( `m_ncorr`, `nombre`, `descripcion`, `link`, 
 	`icono`, `estado`, `orden`, `created`, `usuario`) VALUES
-( 3, 'Ingreso a Bodega', 'Ingreso a Bodega', 'ingreso_bodega.php', '1', 1, 1, '2019-01-25 17:00:35', '1'),
-( 3, 'DevoluciÃ³n a Bodega', 'DevoluciÃ³n a Bodega', 'devolucion_bodega.php', '1', 1, 2, '2019-01-25 17:01:00', '1'),
-( 3, 'Ajuste (+)', 'Ajuste (+)', 'ajuste_positivo.php', '1', 1, 3, '2019-01-25 17:01:24', '1'),
-( 4, 'Ingreso a Vendedor', 'Ingreso a Vendedor', 'ingreso_vendedor.php', '1', 1, 1, '2019-01-25 17:01:51', '1'),
-( 4, 'Cuenta Personal', 'Cuenta Personal', 'cuenta_personal.php', '1', 1, 2, '2019-01-25 17:02:29', '1'),
-( 4, 'Ajuste (-)', 'Ajuste (-)', 'ajuste_negativo.php', '1', 1, 3, '2019-01-25 17:02:48', '1'),
-( 5, 'Toma Inventario Total', 'Toma Inventario Total', 'toma_inventario_total.php', '1', 1, 1, '2019-01-25 17:03:28', '1'),
+( 3, 'Ingreso a Bodega', 'Ingreso a Bodega', 'ingreso_bodega.php', '1', 1, 1, '2019-01-25 17:00:35', '1');
+INSERT INTO `menus_hijos` ( `m_ncorr`, `nombre`, `descripcion`, `link`, 
+	`icono`, `estado`, `orden`, `created`, `usuario`) VALUES
+( 3, 'DevoluciÃ³n a Bodega', 'DevoluciÃ³n a Bodega', 'devolucion_bodega.php', '1', 1, 2, '2019-01-25 17:01:00', '1');
+INSERT INTO `menus_hijos` ( `m_ncorr`, `nombre`, `descripcion`, `link`, 
+	`icono`, `estado`, `orden`, `created`, `usuario`) VALUES
+( 3, 'Ajuste (+)', 'Ajuste (+)', 'ajuste_positivo.php', '1', 1, 3, '2019-01-25 17:01:24', '1');
+INSERT INTO `menus_hijos` ( `m_ncorr`, `nombre`, `descripcion`, `link`, 
+	`icono`, `estado`, `orden`, `created`, `usuario`) VALUES
+( 4, 'Ingreso a Vendedor', 'Ingreso a Vendedor', 'ingreso_vendedor.php', '1', 1, 1, '2019-01-25 17:01:51', '1');
+INSERT INTO `menus_hijos` ( `m_ncorr`, `nombre`, `descripcion`, `link`, 
+	`icono`, `estado`, `orden`, `created`, `usuario`) VALUES
+( 4, 'Cuenta Personal', 'Cuenta Personal', 'cuenta_personal.php', '1', 1, 2, '2019-01-25 17:02:29', '1');
+INSERT INTO `menus_hijos` ( `m_ncorr`, `nombre`, `descripcion`, `link`, 
+	`icono`, `estado`, `orden`, `created`, `usuario`) VALUES
+( 4, 'Ajuste (-)', 'Ajuste (-)', 'ajuste_negativo.php', '1', 1, 3, '2019-01-25 17:02:48', '1');
+INSERT INTO `menus_hijos` ( `m_ncorr`, `nombre`, `descripcion`, `link`, 
+	`icono`, `estado`, `orden`, `created`, `usuario`) VALUES
+( 5, 'Toma Inventario Total', 'Toma Inventario Total', 'toma_inventario_total.php', '1', 1, 1, '2019-01-25 17:03:28', '1');
+INSERT INTO `menus_hijos` ( `m_ncorr`, `nombre`, `descripcion`, `link`, 
+	`icono`, `estado`, `orden`, `created`, `usuario`) VALUES
 ( 5, 'Detalle Toma Inventario', 'Detalle Toma Inventario', 'detalle_toma_inventario.php', '1', 1, 2, '2019-01-25 17:03:56', '1');
 
