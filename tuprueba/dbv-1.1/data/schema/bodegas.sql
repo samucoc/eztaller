08e7cb170b4d9763f6fe0ca8d64eb509014abcbb
samucoc <samu.silva@gmail.com>
2019-01-25 12:15:13 -0300

    upd dbv-1.1

---
+++ tuprueba/dbv-1.1/data/schema/bodegas.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `bodegas` (
+  `b_ncorr` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` text NOT NULL,
+  PRIMARY KEY (`b_ncorr`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
---
+++ tuprueba/dbv-1.1/data/schema/perfiles.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `perfiles` (
+  `p_ncorr` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` text NOT NULL,
+  `descripcion` text NOT NULL,
+  PRIMARY KEY (`p_ncorr`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
