ff370474af7da5991f9d5bda6d921c74927085bb
samucoc <samu.silva@gmail.com>
2019-01-26 11:24:41 -0300

    Create 112.sql

---
+++ tuprueba/dbv-1.1/data/revisions/112/112.sql
@@ -0,0 +1,2 @@
+UPDATE `movim_tipos` SET `nombre` = 'Retiro de bodega' WHERE `movim_tipos`.`mt_ncorr` = 5;
+UPDATE `movim_tipos` SET `descripcion` = 'Retiro de bodega' WHERE `movim_tipos`.`mt_ncorr` = 5;
