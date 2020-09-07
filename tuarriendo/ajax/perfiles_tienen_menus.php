ad299cbfbc5f4bcd93953adb0b896889b35a4eb5
samucoc <samu.silva@gmail.com>
2020-09-06 22:43:16 -0300

    upd tuprueba to tuarriendo

---
+++ tuarriendo/ajax/perfiles_tienen_menus.php
@@ -0,0 +1,129 @@
+<?php
+
+    include "../config/config.php";//Contiene funcion que conecta a la base de datos
+    
+    $action = (isset($_REQUEST['action']) && $_REQUEST['action'] !=NULL)?$_REQUEST['action']:'';
+    if (isset($_GET['id'])){
+        $id_expence=intval($_GET['id']);
+        $query=mysqli_query($con, "SELECT * from perfiles_tienen_menus where ptm_ncorr='".$id_expence."'");
+        $count=mysqli_num_rows($query);
+            if ($delete1=mysqli_query($con,"DELETE FROM perfiles_tienen_menus WHERE ptm_ncorr='".$id_expence."'")){
+            ?>
+            <div class="alert alert-success alert-dismissible" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <strong>Aviso!</strong> Datos eliminados exitosamente.
+            </div>
+            <?php 
+        }else {
+            ?>
+            <div class="alert alert-danger alert-dismissible" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <strong>Error!</strong> Lo siento algo ha salido mal intenta nuevamente.
+            </div>
+<?php
+        } //end else
+    } //end if
+?>
+
+<?php
+    if($action == 'ajax'){
+        // escaping, additionally removing everything that could be (html/javascript-) code
+        $q = mysqli_real_escape_string($con,(strip_tags($_REQUEST['q'], ENT_QUOTES)));
+        $aColumns = array('ptm','perfil', 'menu');//Columnas de busqueda
+        $sTable = "perfiles_tienen_menus";
+        $sWhere = "";
+        if ( isset($q) && $q!='undefined' && $_GET['q'] != "" )
+        {
+            $sWhere = "WHERE (";
+            for ( $i=0 ; $i<count($aColumns) ; $i++ )
+            {
+                $sWhere .= $aColumns[$i]." LIKE '%".isset($q) ? $q : ''."%' OR ";
+            }
+            $sWhere = substr_replace( $sWhere, "", -3 );
+            $sWhere .= ')';
+        }
+        $sWhere.=" order by ptm_ncorr";
+        
+        include 'pagination.php'; //include pagination file
+        //pagination variables
+        $page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
+        $per_page = 10; //how much records you want to show
+        $adjacents  = 4; //gap between pages after number of adjacents
+        $offset = ($page - 1) * $per_page;
+        //Count the total number of row in your table*/
+        $count_query   = mysqli_query($con, "SELECT coalesce(count(*),0) AS numrows FROM $sTable $sWhere");
+        $row= mysqli_fetch_array($count_query);
+        $numrows = $row['numrows'];
+        $total_pages = ceil($numrows/$per_page);
+        $reload = './perfiles_tienen_menus.php';
+        //main query to fetch the data
+        $sql="SELECT * FROM  $sTable $sWhere LIMIT $offset,$per_page";
+        $query = mysqli_query($con, $sql);
+        //loop through fetched data
+        if ($numrows>0){
+            
+            ?>
+            <table class="table table-striped jambo_table bulk_action">
+                <thead>
+                    <tr class="headings">
+                        <th class="column-title">Id </th>
+                        <th class="column-title">Perfil </th>
+                        <th class="column-title">Menú</th>
+                        <th class="column-title no-link last"><span class="nobr"></span></th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php 
+                        while ($r=mysqli_fetch_array($query)) {
+                            $id=$r['ptm_ncorr'];
+                            $query_perfil=mysqli_query($con,"select nombre
+                                                        from perfiles
+                                                        where p_ncorr = '".$r['perfil']."'");
+                            $str_perfil = 'NA';
+                            while ($row=mysqli_fetch_array($query_perfil)) {
+                                $str_perfil=$row['nombre'];
+                            }
+                            $p_ncorr=$r['perfil'];
+                            
+                            $query_menu=mysqli_query($con,"select nombre
+                                                        from menus
+                                                        where m_ncorr = '".$r['menu']."'");
+                            $str_menu = 'NA';
+                            while ($row=mysqli_fetch_array($query_menu)) {
+                                $str_menu=$row['nombre'];
+                            }
+                            $m_ncorr=$r['menu'];
+                            
+                ?>
+                    <input type="hidden" value="<?php echo $p_ncorr;?>" id="p_ncorr<?php echo $id;?>">
+                    <input type="hidden" value="<?php echo $m_ncorr;?>" id="m_ncorr<?php echo $id;?>">
+
+                    <tr class="even pointer">
+                        <td><?php echo ($id);?></td>
+                        <td><?php echo ($str_perfil);?></td>
+                        <td><?php echo ($str_menu);?></td>
+                        <td ><span class="pull-right">
+                        <a href="#" class='btn btn-default' title='Editar Perfil' onclick="obtener_datos('<?php echo $id;?>');" data-toggle="modal" data-target=".bs-example-modal-lg-upd"><i class="glyphicon glyphicon-edit"></i></a> 
+                        <a href="#" class='btn btn-default' title='Borrar Perfil' onclick="eliminar('<?php echo $id; ?>')"><i class="glyphicon glyphicon-trash"></i> </a></span></td>
+                    </tr>
+                <?php
+                    } //end while
+                ?>
+                <tr>
+                    <td colspan=6><span class="pull-right">
+                        <?php echo paginate($reload, $page, $total_pages, $adjacents);?>
+                    </span></td>
+                </tr>
+              </table>
+            </div>
+            <?php
+        }else{
+           ?> 
+            <div class="alert alert-warning alert-dismissible" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <strong>Aviso!</strong> No hay datos para mostrar
+            </div>
+        <?php    
+        }
+    }
+?>
---
+++ tuarriendo/modal/upd_perfiles_tienen_menus_hijos.php
@@ -0,0 +1,69 @@
+
+
+    <div class="modal fade bs-example-modal-lg-upd" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
+                    </button>
+                    <h4 class="modal-title" id="myModalLabel">Editar Menús Hijos</h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal form-label-left input_mask" id="upd_user" name="upd_user">
+                        <div id="result_user"></div>
+                        <input type="hidden" id="mod_id" name="mod_id">
+                        <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
+                            <select name="mod_p_ncorr" id="mod_p_ncorr" required type="text" class="form-control" placeholder="Perfiles">
+                                <?php 
+                                    $query_papa=mysqli_query($con,"select nombre, p_ncorr
+                                                                        from perfiles");
+                                    while ($row_papa=mysqli_fetch_array($query_papa)) {
+                                                ?>
+                                    <option value="<?php echo $row_papa['p_ncorr']?>"><?php echo ($row_papa['nombre'])?></option>
+                                <?php }?>
+                            </select>    
+                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
+                        </div>
+                        
+                        <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
+                            <select name="mod_m_ncorr" id="mod_m_ncorr" required type="text" class="form-control" placeholder="Menús">
+                                <?php 
+                                    $query_papa=mysqli_query($con,"select nombre, m_ncorr
+                                                                        from menus
+                                                                        where estado = '1'");
+                                    while ($row_papa=mysqli_fetch_array($query_papa)) {
+                                                ?>
+                                    <option value="<?php echo $row_papa['m_ncorr']?>"><?php echo ($row_papa['nombre'])?></option>
+                                <?php }?>
+                            </select>    
+                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
+                        </div>
+                        
+                        <div class="col-md-12 col-sm-12 col-xs-12 form-group has-feedback">
+                            <select name="mod_mh_ncorr" id="mod_mh_ncorr" required type="text" class="form-control" placeholder="Menús Hijos">
+                                <?php 
+                                    $query_papa=mysqli_query($con,"select nombre, mh_ncorr
+                                                                        from menus_hijos
+                                                                        where estado = '1'");
+                                    while ($row_papa=mysqli_fetch_array($query_papa)) {
+                                                ?>
+                                    <option value="<?php echo $row_papa['mh_ncorr']?>"><?php echo ($row_papa['nombre'])?></option>
+                                <?php }?>
+                            </select>    
+                            <span class="fa fa-user form-control-feedback right" aria-hidden="true"></span>
+                        </div>
+                        
+                        <div class="ln_solid"></div>
+                        <div class="form-group">
+                            <div class="col-md-9 col-sm-9 col-xs-12 col-md-offset-3">
+                              <button id="upd_data" type="submit" class="btn btn-success">Guardar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div> <!-- /Modal -->
