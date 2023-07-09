f96d49783bd935c7bb7c8ec1691ebdc937514016
Samuel Silva <ssilva@actis.cl>
2023-07-09 01:00:05 -0400

    add proys

---
+++ proys/tumarket/vistas/modulos/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; <?php echo date("Y"); ?> <a href="https://www.eztaller.cl" target="_blank">Eztaller</a>.</strong>
+
+	Todos los derechos reservados.
+
+
+</footer>
