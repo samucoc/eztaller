89c22e7fe34fa382fc1a44ea706e6e3c1ffaec50
Samuel Silva <ssilva@actis.cl>
2024-06-03 15:51:42 -0400

    fix component

--- proys/admgescol/front/src/components/Resumen.jsx
+++ proys/admgescol/front/src/components/Resumen.jsx
@@ -11,7 +11,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const Resumen = ({ userDNI, empresaId }) => {
   const [worker, setWorker] = useState(null);
@@ -20,7 +23,12 @@ const Resumen = ({ userDNI, empresaId }) => {
   const [others, setOthers] = useState([]);
   const [previewPdf, setPreviewPdf] = useState(null);
   const [expanded, setExpanded] = useState('panel1'); // Estado para el acordeón expandido
-
+  const [empresas, setEmpresas] = useState([]);
+  const [usuarios, setUsuarios] = useState([]);
+  const [cargos, setCargos] = useState([]);
+  const [sexos, setSexos] = useState([]);
+  const [comunas, setComunas] = useState([]);
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -39,8 +47,58 @@ const Resumen = ({ userDNI, empresaId }) => {
         console.error('Error fetching data', error);
       }
     };
+    const fetchEmpresas = async () => {
+      try {
+        const response = await axios.get(`${API_BASE_URL}/empresas`);
+        setEmpresas(response.data);
+      } catch (error) {
+        console.error('Error al obtener la lista de empresas:', error);
+      }
+    };
+  
+    const fetchUsuarios = async () => {
+      try {
+        const response = await axios.get(`${API_BASE_URL}/users`);
+        setUsuarios(response.data);
+      } catch (error) {
+        console.error('Error al obtener la lista de usuarios:', error);
+      }
+    };
+  
+    const fetchCargos = async () => {
+      try {
+        const response = await axios.get(`${API_BASE_URL}/cargos`);
+        setCargos(response.data);
+      } catch (error) {
+        console.error('Error al obtener la lista de cargos:', error);
+      }
+    };
+  
+    const fetchSexos = async () => {
+      try {
+        const response = await axios.get(`${API_BASE_URL}/sexo`);
+        setSexos(response.data);
+      } catch (error) {
+        console.error('Error al obtener la lista de sexos:', error);
+      }
+    };
+  
+    const fetchComunas = async () => {
+      try {
+        const response = await axios.get(`${API_BASE_URL}/comunas`);
+        setComunas(response.data);
+      } catch (error) {
+        console.error('Error al obtener la lista de comunas:', error);
+      }
+    };
 
     fetchData();
+    fetchEmpresas();
+    fetchUsuarios();
+    fetchCargos();
+    fetchSexos();
+    fetchComunas();
+
   }, [userDNI, empresaId]);
 
   if (!worker) {
@@ -67,10 +125,344 @@ const Resumen = ({ userDNI, empresaId }) => {
               .slice(0,1)
               .map(d => (
                 <div key={d.id}>
-                  <strong>Nombre Completo:</strong> {d.nombres} {d.apellido_paterno} {d.apellido_materno} <br />
-                  <strong>Rut:</strong> {d.rut}-{d.dv}<br />
-                  <strong>Teléfono:</strong> {d.telefono}<br />
-                  <strong>Email:</strong> {d.email}
+                  <Grid container spacing={2} alignItems="center">
+                    <Grid item xs={12} sm={6}>
+                    <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="empresa_id"
+                        label="Empresa"
+                        name="empresa_id"
+                        select
+                        value={d.empresa_id}
+                        onChange={handleChange}
+                      >
+                      {empresaId === ''
+                        ? empresas.map((empresa) => (
+                            <MenuItem
+                              key={empresa.id}
+                              value={empresa.id}
+                              selected={worker && empresa.id === worker.empresa_id}
+                            >
+                              {empresa.RazonSocial}
+                            </MenuItem>
+                          ))
+                        : empresas
+                            .filter((empresa) => empresa.id === empresaId)
+                            .map((empresa) => (
+                              <MenuItem
+                                key={empresa.id}
+                                value={empresa.id}
+                                selected={worker && empresa.id === worker.empresa_id}
+                              >
+                                {empresa.RazonSocial}
+                              </MenuItem>
+                            ))}
+                      </TextField>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="user_id"
+                        label="Usuario"
+                        name="user_id"
+                        select
+                        value={d.user_id}
+                        onChange={handleChange}
+                      >
+                        {usuarios.map((usuario) => (
+                          <MenuItem key={usuario.id} value={usuario.id} selected={worker && usuario.id === worker.user_id}>
+                            {usuario.userEmail}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="rut"
+                        label="Rut"
+                        name="rut"
+                        value={d.rut}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="dv"
+                        label="Dv"
+                        name="dv"
+                        value={d.dv}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="apellido_paterno"
+                        label="Apellido Paterno"
+                        name="apellido_paterno"
+                        value={d.apellido_paterno}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="apellido_materno"
+                        label="Apellido Materno"
+                        name="apellido_materno"
+                        value={d.apellido_materno}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="nombres"
+                        label="Nombres"
+                        name="nombres"
+                        value={d.nombres}
+                        onChange={handleChange}
+                      />
+                    </Grid>       
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="nombre_social"
+                        label="Nombre Social"
+                        name="nombre_social"
+                        value={d.nombre_social}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="fecha_nac"
+                        label="Fecha de Nacimiento"
+                        name="fecha_nac"
+                        type="date"
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        value={d.fecha_nac}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="nacionalidad"
+                        label="Nacionalidad"
+                        name="nacionalidad"
+                        value={d.nacionalidad}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="cargo_id"
+                        label="Cargo"
+                        name="cargo_id"
+                        select
+                        value={d.cargo_id}
+                        onChange={handleChange}
+                      >
+                        {cargos.map((cargo) => (
+                          <MenuItem key={cargo.id} value={cargo.id} selected={worker && cargo.id === worker.cargo_id}>
+                            {cargo.nombre}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </Grid>      
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="sexo_id"
+                        label="Sexo"
+                        name="sexo_id"
+                        select
+                        value={d.sexo_id}
+                        onChange={handleChange}
+                      >
+                        {sexos.map((sexo) => (
+                          <MenuItem key={sexo.id} value={sexo.id} selected={worker && sexo.id === worker.sexo_id}>
+                            {sexo.nombre}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        type="file"
+                        id="foto"
+                        label="foto"
+                        name="foto"
+                        value={d.foto}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="direccion"
+                        label="Dirección"
+                        name="direccion"
+                        value={d.direccion}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="comuna_id"
+                        label="Comuna"
+                        name="comuna_id"
+                        select
+                        value={d.comuna_id}
+                        onChange={handleChange}
+                      >
+                        {comunas.map((comuna) => (
+                          <MenuItem key={comuna.id} value={comuna.id} selected={worker && comuna.id === worker.comuna_id}>
+                            {comuna.nombre}
+                          </MenuItem>
+                        ))}
+                      </TextField>
+                    </Grid>        
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="telefono"
+                        label="Teléfono"
+                        name="telefono"
+                        value={d.telefono}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="email"
+                        label="Email"
+                        name="email"
+                        value={d.email}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="contacto_emergencia"
+                        label="Contacto Emergencia"
+                        name="contacto_emergencia"
+                        value={d.contacto_emergencia}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="telefono_emergencia"
+                        label="Teléfono Emergencia"
+                        name="telefono_emergencia"
+                        value={d.telefono_emergencia}
+                        onChange={handleChange}
+                      />
+                    </Grid>        
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        variant="outlined"
+                        InputProps={{
+                          readOnly: true,
+                        }}
+                        fullWidth
+                        id="estado_id"
+                        label="Estado"
+                        name="estado_id"
+                        value={d.estado_id}
+                        onChange={handleChange}
+                      />
+                    </Grid>
+                  </Grid>
                 </div>
               ))}
           </Typography>
