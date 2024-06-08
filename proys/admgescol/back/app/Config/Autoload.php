18d20b19e0668c269dd273dff872f9265ed393ce
Samuel Silva <ssilva@actis.cl>
2024-06-07 23:05:45 -0400

    fix build

--- proys/admgescol/back/app/Config/Autoload.php
+++ proys/admgescol/back/app/Config/Autoload.php
@@ -42,6 +42,8 @@ class Autoload extends AutoloadConfig
     public $psr4 = [
         APP_NAMESPACE => APPPATH, // For custom app namespace
         'Config'      => APPPATH . 'Config',
+        'Smalot\PdfParser' => ['vendor/Smalot/PdfParser/'],
+
     ];
 
     /**
