b9bd57aef523dd3fb4d9d648c47782cc5f4567e2
Samuel Silva <ssilva@actis.cl>
2024-09-20 19:03:50 -0300

    fix build

--- proys/admgescol/front/src/components/config/apiConstants.js
+++ proys/admgescol/front/src/components/config/apiConstants.js
@@ -1,8 +1,8 @@
-//export const API_BASE_URL = 'https://admgescol.eztaller.cl/back/public/Api/V1';
-//export const API_DOWNLOAD_URL = 'https://admgescol.eztaller.cl/back/public';
+export const API_BASE_URL = 'https://admgescol.eztaller.cl/back/public/Api/V1';
+export const API_DOWNLOAD_URL = 'https://admgescol.eztaller.cl/back/public';
 
-export const API_DOWNLOAD_URL = 'http://localhost/eztaller/proys/admgescol/back/public';
-export const API_BASE_URL = 'http://localhost/eztaller/proys/admgescol/back/public/Api/V1';
+//export const API_DOWNLOAD_URL = 'http://localhost/eztaller/proys/admgescol/back/public';
+//export const API_BASE_URL = 'http://localhost/eztaller/proys/admgescol/back/public/Api/V1';
  
 
  
